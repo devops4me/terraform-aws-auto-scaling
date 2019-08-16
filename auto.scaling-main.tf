0134dc8c274bd96cbeb6ab1a64d251aa8934fa59
Apollo Akora <apolloakora@gmail.com>
2019-08-16 09:54:31 +0100

    have documented the auto scaling group and launch config resources

--- auto.scaling-main.tf
+++ auto.scaling-main.tf
@@ -1,4 +1,14 @@
 
+/*
+ | --
+ | -- When the clustered ec2 instances are reaching the end of their
+ | -- tether this auto scaling group (and launch configuration) sees
+ | -- to it that more are automatically provisioned.
+ | --
+ | -- Dynamic tags are used here because the auto-scaling group will
+ | -- be responsible for placing tags on the provisioned ec2 intances.
+ | --
+*/
 resource aws_autoscaling_group cluster {
 
     name                 = "${ local.ecosystem_name }-asg-${ module.resource-tags.out_tag_timestamp }"
@@ -24,7 +34,7 @@ resource aws_autoscaling_group cluster {
 
     dynamic tag {
 
-        for_each = local.mandatory_tags
+        for_each = var.in_mandatory_tags
         content {
             key   = tag.key
             value = tag.value
@@ -34,6 +44,18 @@ resource aws_autoscaling_group cluster {
 }
 
 
+/*
+ | --
+ | -- This launch configuration ensures that
+ | --
+ | --   a) a given AMI is used to raise the instances
+ | --   b) the instance type is of a particular ilk
+ | --   c) the ec2 instance adopts a role profile carrying access policies
+ | --   d) each instance is plied with a public SSH key
+ | --   e) the security group for each instance is the one stated
+ | --   f) the user data scripts are run after the instance boots up
+ | --
+*/
 resource aws_launch_configuration cluster {
 
     name_prefix          = "${ local.ecosystem_name }-launch-config-${ module.resource-tags.out_tag_timestamp }"
@@ -52,6 +74,17 @@ resource aws_launch_configuration cluster {
 }
 
 
+/*
+ | --
+ | -- The ec2 instances fashioned by the launch configuration are
+ | -- plied with this public key to enable troubleshooters to go in
+ | -- and diagnose issues.
+ | --
+ | -- Best practise is to block ssh on production systems but if
+ | -- and when you need to get out of jail free, you place a security
+ | -- group rule allowing it and proceed to perform diagnosis.
+ | --
+*/
 resource aws_key_pair ssh {
     key_name = "key-4-${ local.ecosystem_name }-${ module.resource-tags.out_tag_timestamp }"
     public_key = "${ var.in_ssh_public_key }"
--- auto.scaling-variables.tf
+++ auto.scaling-variables.tf
@@ -1,3 +1,45 @@
 
-## Auto Scaling Variables
+################ ############################################### ########
+################ Module [[[auto-scaling]]] Input Variables List. ########
+################ ############################################### ########
 
+### ############################## ###
+### [[variable]] in_mandatory_tags ###
+### ############################## ###
+
+variable in_mandatory_tags {
+
+    description = "Optional tags unless your organization mandates that a set of given tags must be set."
+    type        = "map"
+    default     = { }
+}
+
+
+### ################# ###
+### in_ecosystem_name ###
+### ################# ###
+
+variable in_ecosystem_name {
+
+    description = "Creational stamp binding all infrastructure components created on behalf of this ecosystem instance."
+}
+
+
+### ################ ###
+### in_tag_timestamp ###
+### ################ ###
+
+variable in_tag_timestamp {
+
+    description = "A timestamp for resource tags in the format ymmdd-hhmm like 80911-1435"
+}
+
+
+### ################## ###
+### in_tag_description ###
+### ################## ###
+
+variable in_tag_description {
+
+    description = "Ubiquitous note detailing who, when, where and why for every infrastructure component."
+}
